c836886387a53209b19c86e1742cbee8f005d338
Niyi <princzny@gmail.com>
2020-05-29 10:09:46 +0100

    more updates

--- anime.js
+++
@@ -1,6 +0,0 @@
-anime({
- targets : '.mid-content',
- opacity : 0,
- duration: 200
-});
-console.log('mid-content');
--- app.js
+++ app.js
@@ -1,56 +1,27 @@
-// Slide for nav bar menu button
-// let navSlide = ()=> {
-//     let  burger = document.querySelector('.burger');
-//     let nav = document.querySelector('.nav-links');
-//     let closeIcon = document.querySelector("img.menu-close");
-//     let navLinks = document.querySelectorAll('.nav-links li');
 
-//     //Nav Toggler for menu
-//     burger.addEventListener('click', ()=> {
-//         nav.classList.toggle('nav-active');
-//     });
-//     for (let x = 0; x < closeIcon.length; x++) {
-    
-    
-//         closeIcon[x].addEventListener("click", function () {
-            
-//             burger.classList.remove("ul");
-//             nav.classList.remove("ul");
-    
-    
-//         });
-        
-//     }
-    
+// const goToTopButton = document.querySelector('.gotopbtn');
+
+// window.addEventListener('scroll', scrollFunction);
+
+// function scrollFunction() {
+// if (window.pageYOffset > 100) { // goToTopButton
+//     goToTopButton.style.display = "block";
 // }
-// // Slide close 
-// navSlide();
-
-//scroll function for window
-const goToTopButton = document.querySelector('.gotopbtn');
-
-window.addEventListener('scroll', scrollFunction);
-
-function scrollFunction() {
-if (window.pageYOffset > 100) { // goToTopButton
-    goToTopButton.style.display = "block";
-}
-else{
-    goToTopButton.style.display = "none";
-}
-}
+// else{
+//     goToTopButton.style.display = "none";
+// }
+// }
 
 
 
 
-$(".txtb input").on("focus", function(){
-    $(this).addClass("focus");
- });
- Element.prototype.documentOffsetTop = function () {
-    return this.offsetTop + ( this.offsetParent ? this.offsetParent.documentOffsetTop() : 0 );
-};
+// $(".txtb input").on("focus", function(){
+//     $(this).addClass("focus");
+//  });
+//  Element.prototype.documentOffsetTop = function () {
+//     return this.offsetTop + ( this.offsetParent ? this.offsetParent.documentOffsetTop() : 0 );
+// };
 
-// var top = document.getElementsByClassName( '.about' ).documentOffsetTop() - ( window.innerHeight / 2 );
-// window.scrollTo( 0, top );
+
 
 
